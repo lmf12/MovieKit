fcb001e1b93349632715d4e27248e259bfdb4711
Lyman Li <limianfeng12@gmail.com>
2022-09-11 13:02:11 +0800

    添加 clip 和 composition

---
+++ MovieKit/Core/Clip.swift
@@ -0,0 +1,23 @@
+//
+//  Clip.swift
+//  MovieKitDemo
+//
+//  Created by Lyman Li on 2022/9/11.
+//  Copyright © 2022 Lyman Li. All rights reserved.
+//
+
+import AVFoundation
+
+class Clip {
+    var startTime : CMTime = .zero
+    var timeRange : CMTimeRange {
+        get {
+            return CMTimeRange(start: .zero, duration: self.asset.duration)
+        }
+    }
+    private(set) var asset : AVAsset
+    
+    init(asset : AVAsset) {
+        self.asset = asset
+    }
+}
---
+++ MovieKit/Core/Composition.swift
@@ -0,0 +1,30 @@
+//
+//  Composition.swift
+//  MovieKitDemo
+//
+//  Created by Lyman Li on 2022/9/11.
+//  Copyright © 2022 Lyman Li. All rights reserved.
+//
+
+import AVFoundation
+
+class Composition {
+    private(set) var composition : AVComposition
+    
+    init(composition : AVComposition) {
+        self.composition = composition
+    }
+
+    // MARK: - Public
+    
+    // 创建可播放项目
+    public func createPlayerItem() -> AVPlayerItem {
+        return AVPlayerItem(asset: composition.copy() as! AVAsset)
+    }
+    
+    // 创建可导出项目
+    public func createAssetExportSession(exportPreset : String?) -> AVAssetExportSession? {
+        return AVAssetExportSession(asset: composition.copy() as! AVAsset,
+                                    presetName: exportPreset ?? AVAssetExportPresetHighestQuality)
+    }
+}
